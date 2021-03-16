01a2aab0912814a71a600e5b5c938e8925208580
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-03-16 12:29:39 +0800

    Initial commit

---
+++ src/main/java/cn/com/xuxiaowei/electron/updater/UpdaterApplication.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.electron.updater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpdaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/com/xuxiaowei/electron/updater/UpdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.electron.updater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
