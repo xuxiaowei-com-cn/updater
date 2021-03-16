2cd51ad2f5b71eb469b957b7d9f8398673a003a2
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-03-16 12:30:26 +0800

    Update UpdaterApplication.java

--- src/main/java/cn/com/xuxiaowei/electron/updater/UpdaterApplication.java
+++ src/main/java/cn/com/xuxiaowei/electron/updater/UpdaterApplication.java
@@ -3,6 +3,12 @@ package cn.com.xuxiaowei.electron.updater;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 程序执行入口
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
 @SpringBootApplication
 public class UpdaterApplication {
 
